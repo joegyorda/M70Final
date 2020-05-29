1a7c24b2a5e19f6515959f0f6d03d0f91bd453fc
Student <student@MacBook-Air-103.local>
2020-05-29 18:03:20 -0400

    changed testing and training functions

--- m70final.R
+++ m70final.R
@@ -12,7 +12,7 @@ require(bizdays)
 
 main = function(backtest=1,capital=100000){
   d = parse.data("all_stocks_5yr.csv")
-  cross_val(model=hclust.portfolio, backtest=backtest,d,capital)
+  cross_val(backtest=backtest,d,capital)
 }
 
 
@@ -38,7 +38,7 @@ parse.data = function(path){
   return(res)
 }
 
-cross_val = function(model=hclust.portfolio, backtest=1, d, capital){
+cross_val = function(backtest=1, d, capital){
   #First start date for training
   first = ymd(names(d)[2])
   #Determine last start date for training
@@ -50,7 +50,7 @@ cross_val = function(model=hclust.portfolio, backtest=1, d, capital){
 
   
   #Train on 6 months, test on 6 months, no redistribution
-  if(backtest==1 | backtest==2){
+  if(backtest==1){
 
     # Start dates for training (also end dates for testing) using biz days of NYSE
     starts.train = bizseq(from=first, to=last,'QuantLib/UnitedStates/NYSE')
@@ -64,52 +64,74 @@ cross_val = function(model=hclust.portfolio, backtest=1, d, capital){
     ends.test = add_with_rollback(starts.test, months(6), roll_to_first = TRUE)
     ends.test = adjust.next(ends.test,'QuantLib/UnitedStates/NYSE')
     
-    #Train on 6 months, test on 6 months, redistribute at 3 months
-    if(backtest==2){
-      mids.test = add_with_rollback(starts.test, months(3), roll_to_first = TRUE)
-      mids.test = adjust.next(ends.test,'QuantLib/UnitedStates/NYSE')
-    }
-    
-    # Matrix of returns by stock by training set (6 month period)
-    size= length(starts.train)*nrow(d)
-    #returnf = matrix(rep(0,size), nrow=nrow(d))
-    returnf = c()
+    returns = c()
     for(i in 1:(length(starts.train))){
 
-      # Determine weights
-      weights.clust = model(d %>% select(format(starts.train[i]):format(ends.train[i])))
-      # Determine how much of each stock we buy
-
-      bought = weights.clust*capital
-      # Change in stock price
-
-      #print(ends.test[i])
-      old_price = d %>% select(format(starts.test[i]))
-      new_price = d %>% select(format(ends.test[i]))
-      if(backtest==1){ 
-        change = (new_price - old_price) / (old_price)
-        returnf=  c(returnf,(sum((bought * change)[[1]]))/capital)
-      }
-      
-      if(backtest==2){
-        mid_price = d %>% select(format(mids.test[i]))
-        mid_change = (old_price - mid_price)/old_price
-        mid_bought = weights.clust*sum((bought * mid_change)[[1]])
-        
-        change = (new_price - mid_price)/mid_price
-        returnf=  c(returnf,(sum((mid_bought * mid_change)[[1]]))/capital)
-      }
-      #returnf[,i] <- (bought * change)[[1]]
     }
-    #View(returnf[ , (length(returnf[1,])-10):length(returnf[1,])])
   }
 
-  #Train only on first six months, test on increasing intervals of time
   if(backtest==3){
-    ends.train = seq(seq(first, by="6 months", length.out=2)[2], by="3 year",length.out=20)
+    
   }
 
 
+}
+
+### Calculate weights for training period
+# Start - start of training period - date object
+# End - end of training period - date object
+
+calc.weights = function(start, end, d, returnLow=0, returnHigh=0){
+  # rows are models, columns are stocks
+  # Including intervalPortfolio
+  #weights = matrix(rep(0,3*nrow(d)),nrow=3)
+  # Not inclduing interval portfolio
+  weights = matrix(rep(0,2*nrow(d)),nrow=2)
+  
+  weights[1,] = hclust.portfolio(d %>% select(format(starts.train[i]):format(ends.train[i])))
+  weights[2,] = markBullet(d %>% select(format(starts.train[i]):format(ends.train[i])))
+  #weights[3,] = intervalPortfolio(d %>% select(format(starts.train[i]):format(ends.train[i])), r=returnLow, R=returnHigh)
+  return(weights)
+}
+
+### Calculate returns for testing period
+# Start - start of testing period - date object
+# End - end of testing period - date object
+# weights - matrix of weights
+# realloc - number of times to reallocate
+# capital - starting capital
+calc.return = function(start,end,weights,capital,realloc){
+  # get start price
+  old_price = d %>% select(format(start))
+  # number of days per time period
+  numDays = (end-start)/realloc
+  
+  for(i in 0:realloc){
+    # Stock bought for time period
+    bought = weights*capital
+    
+    # Find end of time period
+    mid = add_with_rollback(starts.test, days(numDays), roll_to_first = TRUE)
+    mid = adjust.next(ends.test,'QuantLib/UnitedStates/NYSE')
+
+    # Ending prices for time period
+    new_price = d %>% select(format(mid))
+    
+    # Change in stock prices for time period
+    change = (old_price - mid_price)/old_price
+    
+    # Returns for time period
+    # Make sure change is a column vector - stocks as rows 
+    returns = (bought %*% change)#[[1]]
+    
+    # New capital
+    capital = sum(returns)
+    
+    # New prices
+    old_price = new_price
+    
+  }
+  return(capital)
 }
 
 ################  Hierarchical Clustering Optimization ################
